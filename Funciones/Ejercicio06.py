7c08d54184dab0be54d0d743154937500effc8ff
Emiliano Stasi Fernandez <103003739+Emiliano2122@users.noreply.github.com>
2024-04-13 10:48:56 -0600

    feat: menu
    le agregó el menu para que el usuario pueda seleccionar de forma automática que accion quiere realizar sobre que factorial quiere hacer y si ya termino solo debe presionar el numero 9 y así terminara el factorial

--- Funciones/Ejercicio06.py
+++ Funciones/Ejercicio06.py
@@ -3,6 +3,22 @@ def factorial(n):
     for i in range(n):
         tmp *= i+1
     return tmp
-tmp = input("Escribe otro numero en este factorial")
-print(factorial(5))
-print(factorial(34))
+seleccion_de_menu_principal = 0
+menu = True
+
+text_menu = """
+            Progama para calcular un factorial
+            1- Calcular el factorial
+            9- Salir del calculo del factorial
+            """
+while(menu):
+    print(text_menu)
+    try:
+        seleccion_de_menu_principal = int(input("Elije la opcion: "))
+        if(seleccion_de_menu_principal == 1):
+            tmp = int(input("Escribe otro numero en este factorial"))
+            print(factorial(tmp))
+        if(seleccion_de_menu_principal == 9):
+            menu = False
+    except ValueError:
+        print("No es un numero")
